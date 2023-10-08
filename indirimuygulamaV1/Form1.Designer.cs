d381bf596f12e9cada18271786b533db6155b885
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-08 14:47:35 +0300

    Add files via upload

---
+++ indirimuygulamaV1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace indirimuygulamaV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lblİndirimlifiyat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Etiket fiyatı";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(148, 25);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 20);
+            this.txtFiyat.TabIndex = 1;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.IndianRed;
+            this.btn2.Location = new System.Drawing.Point(173, 64);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 50);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Yüzde \r\n  25\r\nİNDİRİM\r\n";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Firebrick;
+            this.btn3.Location = new System.Drawing.Point(25, 135);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 50);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "Yüzde\r\n  50\r\nİNDİRİM\r\n";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Red;
+            this.btn4.Location = new System.Drawing.Point(173, 135);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 50);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "Yüzde\r\n 75\r\nİNDİRİM\r\n";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.LightCoral;
+            this.btn1.Location = new System.Drawing.Point(25, 64);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 50);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "Yüzde\r\n  10\r\nİNDİRİM\r\n";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lblİndirimlifiyat
+            // 
+            this.lblİndirimlifiyat.AutoSize = true;
+            this.lblİndirimlifiyat.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblİndirimlifiyat.Location = new System.Drawing.Point(25, 209);
+            this.lblİndirimlifiyat.Name = "lblİndirimlifiyat";
+            this.lblİndirimlifiyat.Size = new System.Drawing.Size(68, 13);
+            this.lblİndirimlifiyat.TabIndex = 6;
+            this.lblİndirimlifiyat.Text = "indirimli fiyat :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(285, 256);
+            this.Controls.Add(this.lblİndirimlifiyat);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lblİndirimlifiyat;
+    }
+}
+
---
+++ indirimuygulamaV1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace indirimuygulamaV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati=Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.10;
+            lblİndirimlifiyat.Text=indirimliFiyat.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.25;
+            lblİndirimlifiyat.Text = indirimliFiyat.ToString();
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.50;
+            lblİndirimlifiyat.Text = indirimliFiyat.ToString();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            int etiketFiyati;
+            double indirimliFiyat;
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.75;
+            lblİndirimlifiyat.Text = indirimliFiyat.ToString();
+        }
+    }
+}
